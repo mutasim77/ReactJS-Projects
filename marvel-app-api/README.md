1ae444a27a06ec8034a3cf4178fe1aa0c83a6f19
Mutasim <qmutasim77@gmail.com>
2023-07-22 23:21:46 +0500

    marvel project ❤️"

---
+++ marvel-app-api/README.md
@@ -0,0 +1,8 @@
+# Demo
+ - 1
+<img src="ScreenShot-1.png">
+
+<hr>
+
+- 2
+<img src="ScreenShot-2.png">
---
+++ marvel-app-api/src/services/MarvelService.js
@@ -0,0 +1,74 @@
+import { useHttp } from "../hooks/http.hook";
+import { API_KEY, API_HASH } from '../config/config.json';
+
+const useMarvelService = () => {
+	const { loading, request, error, clearError } = useHttp();
+	const _apiBase = "https://gateway.marvel.com:443/v1/public/";
+	const _baseOffset = 210;
+
+	const getAllCharacters = async (offset = _baseOffset) => {
+		const res = await request(
+			`${_apiBase}characters?limit=9&offset=${offset}&${API_KEY}`
+		);
+		return res.data.results.map(_transformCharacter);
+	};
+
+	const getCharacter = async (id) => {
+		const res = await request(`${_apiBase}characters/${id}?${API_KEY}&${API_HASH}`);
+		return _transformCharacter(res.data.results[0]);
+	};
+
+	const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${API_KEY}`
+		);
+		return res.data.results.map(_transformComics);
+	};
+
+	const getComics = async (id) => {
+		const res = await request(`${_apiBase}comics/${id}?${API_KEY}`);
+		return _transformComics(res.data.results[0]);
+	};
+
+	const _transformCharacter = (char) => {
+		return {
+			id: char.id,
+			name: char.name,
+			description: char.description
+				? `${char.description.slice(0, 210)}...`
+				: "There is no description for this character",
+			thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items,
+		};
+	};
+
+	const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		};
+	};
+
+	return {
+		loading,
+		error,
+		clearError,
+		getAllCharacters,
+		getCharacter,
+		getAllComics,
+		getComics,
+	};
+};
+
+export default useMarvelService;
