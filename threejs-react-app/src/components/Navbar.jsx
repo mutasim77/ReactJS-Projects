871cc8b024415a743b75c9de1dd1d011deffc5b5
Mutasim <qmutasim77@gmail.com>
2023-08-20 12:07:56 +0600

    added project

---
+++ threejs-react-app/src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Container = styled.div`
+width: 1250px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    padding: 10px;
+  }
+`;
+
+const Links = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Logo = styled.h2`
+  height: 50px;
+  display: flex;
+  align-items: center;
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #da4ea2;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Navbar = () => {
+  return (
+    <Section>
+      <Container>
+        <Links>
+          <Logo>Mutasim</Logo>
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Studio</ListItem>
+            <ListItem>Works</ListItem>
+            <ListItem>Contact</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          {/* Changed the image due to copyright problems */}
+          <Icon src="./img/search.png" />
+          <Button>Hire Now</Button>
+        </Icons>
+      </Container>
+    </Section>
+  );
+};
+
+export default Navbar;
